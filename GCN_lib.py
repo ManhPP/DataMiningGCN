ad69b8a5a286b1b39e230fb1d6b6943581db3d7d
manhpp <phumanh1998@gmail.com>
2021-09-04 23:38:48 +0700

    update

--- GCN_lib.py
+++ GCN_lib.py
@@ -2,6 +2,7 @@ from torch_geometric.datasets import Planetoid
 import torch
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
+from torch.utils.tensorboard import SummaryWriter
 
 
 dataset = Planetoid(root='data/Cora', name='Cora')
@@ -29,17 +30,38 @@ model = GCN().to(device)
 data = dataset[0].to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
 
-model.train()
+writer = SummaryWriter('./runs/' + model._get_name())
 
-for epoch in range(200):
+
+def train(epoch):
+    model.train()
     optimizer.zero_grad()
     out = model(data)
     loss = F.nll_loss(out[data.train_mask + data.val_mask], data.y[data.train_mask + data.val_mask])
     loss.backward()
     optimizer.step()
 
-model.eval()
-pred = model(data).argmax(dim=1)
-correct = (pred[data.test_mask] == data.y[data.test_mask]).sum()
-acc = int(correct) / int(data.test_mask.sum())
-print('Accuracy: {:.4f}'.format(acc))
+    pred = out.argmax(dim=1)
+    correct = sum(pred[data.train_mask + data.val_mask] == data.y[data.train_mask + data.val_mask])
+    acc = int(correct) / int(sum(data.train_mask + data.val_mask))
+    writer.add_scalar("train/loss", loss, epoch)
+    writer.add_scalar("train/acc", acc, epoch)
+
+
+def test(epoch):
+    model.eval()
+    with torch.no_grad():
+        out = model(data)
+        loss = F.nll_loss(out[data.test_mask], data.y[data.test_mask])
+        pred = out.argmax(dim=1)
+        correct = (pred[data.test_mask] == data.y[data.test_mask]).sum()
+        acc = int(correct) / int(data.test_mask.sum())
+        writer.add_scalar("test/loss", loss, epoch)
+        writer.add_scalar("test/acc", acc, epoch)
+
+
+if __name__ == '__main__':
+    for epoch in range(1, 201):
+        train(epoch)
+        test(epoch)
+        writer.close()
