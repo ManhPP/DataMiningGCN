54355e90a4334e745561a589279d7390537413f8
manhpp <phumanh1998@gmail.com>
2021-09-06 16:57:36 +0700

    update

--- trainer.py
+++ trainer.py
@@ -5,8 +5,8 @@ from torch.utils.tensorboard import SummaryWriter
 
 from model import GCN, GraphUNet
 
-dataset = Planetoid(root='data/Cora', name='Cora')
-
+dataset = Planetoid(root='data/PubMed', name='PubMed')
+best = 0
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # model = GCN(dataset).to(device)
 model = GraphUNet(in_channels=dataset.num_features,
@@ -23,22 +23,27 @@ def train(epoch):
     model.train()
     optimizer.zero_grad()
     out = model(data.x, data.edge_index)
-    loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
+    out = F.normalize(out, dim=1)
+    # mask = data.train_mask + data.val_mask
+    mask = ~data.test_mask
+    loss = F.nll_loss(out[mask], data.y[mask])
     loss.backward()
     optimizer.step()
 
     pred = out.argmax(dim=1)
-    correct = sum(pred[data.train_mask] == data.y[data.train_mask])
-    acc = int(correct) / int(sum(data.train_mask))
+    correct = sum(pred[mask] == data.y[mask])
+    acc = int(correct) / int(sum(mask))
     writer.add_scalar("train/loss", loss, epoch)
     writer.add_scalar("train/acc", acc, epoch)
-    print(f"{epoch}: {acc}")
+    print(f"train {epoch}: {acc}")
 
 
 def test(epoch, mode="val"):
+    global best
     model.eval()
     with torch.no_grad():
         out = model(data.x, data.edge_index)
+        out = F.normalize(out, dim=1)
         mask = data.val_mask
         if mode == "test":
             mask = data.test_mask
@@ -48,11 +53,18 @@ def test(epoch, mode="val"):
         acc = int(correct) / int(mask.sum())
         writer.add_scalar(f"{mode}/loss", loss, epoch)
         writer.add_scalar(f"{mode}/acc", acc, epoch)
+        print(f"{mode} {epoch}: {acc}")
+        if mode == "test":
+            if acc > best:
+                torch.save(model.state_dict(), f"{model._get_name()}_{dataset.name}.pkl")
+                best = acc
+            print("best: ", best)
 
 
 if __name__ == '__main__':
     for epoch in range(1, 201):
         train(epoch)
-        test(epoch)
+        # test(epoch)
         test(epoch, mode="test")
     writer.close()
+    print(best)
